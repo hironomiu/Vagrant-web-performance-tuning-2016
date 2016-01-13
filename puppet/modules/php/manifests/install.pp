981ab0f229ed7b2d07968781e0789b95bfc8a37f
hironomiu <hironomiu@example.com>
2016-01-13 13:35:03 +0900

    インストール時に最新を指定

--- puppet/modules/php/manifests/install.pp
+++ puppet/modules/php/manifests/install.pp
@@ -1,22 +1,4 @@
 class php::install{
-#    yumrepo { 'php-epel':
-#        descr => 'epel repo',
-#        mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=$basearch',
-#        enabled    => 1,
-#        gpgcheck   => 1,
-#        gpgkey     => 'https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7',
-#    }
-#
-#    yumrepo { 'php-remi':
-#        descr => 'remi repo',
-#        mirrorlist     => 'http://rpms.famillecollet.com/enterprise/$releasever/php56/mirror',
-#        enabled        => 0,
-#        gpgcheck       => 1,
-#        gpgkey     => 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi',
-#        priority       => 1,
-#        require => Yumrepo['php-epel'],
-#    }
-
     package{'epel-release':
         provider => 'yum',
         ensure => installed
@@ -25,7 +7,6 @@ class php::install{
     package { 'remi-release':
         ensure   => installed,
         source   => 'http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
-        # source   => 'http://remi.kazukioishi.net/enterprise/remi-release-6.rpm',
         provider => rpm,
         require  => Package['epel-release'],
     }
@@ -51,9 +32,8 @@ class php::install{
         'memcached',
         ]:
         provider => 'yum',
-        ensure => installed,
+        ensure => 'latest',
         install_options => ['--enablerepo=remi,remi-php56'],
-       # require => Yumrepo['php-remi']
         require => Package['remi-release']
     }
 
@@ -77,7 +57,6 @@ class php::install{
         ]:
         provider => 'yum',
         ensure => installed,
-        # require => Yumrepo['php-remi']
         require => Package['remi-release']
     }
 
@@ -86,7 +65,6 @@ class php::install{
         'cronie-anacron',
         ]:
         ensure => purged,
-        # require => Yumrepo['php-remi'],
         require => Package['remi-release']
     }
 
